956bd1f630e40183c5fe746ceb4e18a1f502ed07
26thmeusoc <development@26thmeusoc.com>
2013-03-09 16:56:14 +0100

    Cleaned Charactername Code and added some Documentation

--- src/rogue/system/CharacterCreation.java
+++ src/rogue/system/CharacterCreation.java
@@ -20,35 +20,64 @@ public class CharacterCreation {
 	 * @return Chosen Name of Character
 	 */
 	public static String getCharacterName(TiledTermPanel term, World world) {
+		// Set to some character
 		char c = ' ';
-		String printName = new String("Name: _");
-		String name = new String();
-		while (c != '\n') {
-			Screen.printLine(printName, term, world);
+		// Set Standardinputstring
+		String printName = new String("Name: _"); // Used to Store String to be printed
+		String name = new String(); // Used to Store name given by user
+		while (c != '\n') { // Stop when user presses Return (Newline)
+			Screen.printLine(printName, term, world); // Print Users Input
 			try {
+				// Read next Character
 				c = term.getKey();
 				switch (c) {
+					// User pressed Backspace
 					case '\b':
-						if (name.length() != 0) {
+						// Name has already some Letters?
+						if (name.length() != 0) { // Yes, remove last one
+							// Remove last letter from Name
 							name = name.substring(0,name.length()-1);
+							// Remove last letter from Output
 							printName = printName.substring(0,printName.length()-2).concat("_");
 						}
 						break;
 					case '\n':
+						// Just Jump to start of while loop
 						break;
 					default:
-						if (name.length() < 8) {
-							name = name.concat(new Character(c).toString());
-							printName = printName.substring(0,printName.length()-1).concat(new Character(c).toString()).concat("_");
+						// Has Maximumlength of Name been reached?
+						if (name.length() < 8) { // No
+							// Check what kind of Character has been pressed
+							if (valueIsInIntervall((int)'0',(int)'9',(int)c) ||
+								valueIsInIntervall((int)'a',(int)'z',(int)c) ||
+								valueIsInIntervall((int)'A',(int)'Z',(int)c) ||
+								c == ' ') {
+								// Add to Name
+								name = name.concat(new Character(c).toString());
+								// Delete Input Character and last letter, dann append Input Character again
+								printName = printName.substring(0,printName.length()-1).concat(new Character(c).toString()).concat("_");
+							}
 						}
 						break;
-
 				}
-			} catch(InterruptedException e) {
+
+			} catch(InterruptedException e) { // Something went wrong 
 				System.out.println("!Interrupted Exception");
 				e.printStackTrace();
 			}
 		}
 		return name;
+	}
+
+	/**
+	 * Checks if a Value is between two other Values
+	 *
+	 * @param lowestValue lowest value which is allowed
+	 * @param highestValue highest allowed value
+	 * @param value checked value
+	 * @return Boolean which tells if value is between highest and lowest Value 
+	 */
+	public static boolean valueIsInIntervall(int lowestValue, int highestValue, int value) {
+		return ((value >= lowestValue) && (value <= highestValue));
 	}
 }
