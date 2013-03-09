9bde617dc52e67b0620cd1a412b29006bc885be1
26thmeusoc <development@26thmeusoc.com>
2013-03-09 15:16:13 +0100

    Added file

---
+++ src/rogue/system/CharacterCreation.java
@@ -0,0 +1,25 @@
+/*
+ * CharacterCreation.java
+ * Copyright (C) 2013 dirk <dirk@Valinor.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+package rogue.system;
+
+import jade.ui.TiledTermPanel;
+import jade.core.World;
+
+public class CharacterCreation
+{
+	public CharacterCreation() {
+		
+	}
+
+	public static String getCharacterName(TiledTermPanel term, World world) {
+		char c = '';
+		while (c != '\n') {
+		}
+	}
+}
+
+
