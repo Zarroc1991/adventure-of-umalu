59326db777e60f90924a84cf3e94159dc05acf1a
26thmeusoc <development@26thmeusoc.com>
2013-03-09 11:01:08 +0100

    Added Comments

--- src/rogue/Rogue.java
+++ src/rogue/Rogue.java
@@ -16,46 +16,71 @@ import rogue.system.Path;
 
 public class Rogue {
 	public static void main(String[] args) throws InterruptedException {
+		// How many rounds for next healing+
 		final int hpCycle=10;
 		int roundsToHpUp = hpCycle;
-		// Get current Operating System
-        	TiledTermPanel term = TiledTermPanel.getFramedTerminal("Jade Rogue");
+		// Nobody knows right now, what happens here
         	term.registerTile("dungeon.png", 5, 59, ColoredChar.create('#'));
         	term.registerTile("dungeon.png", 3, 60, ColoredChar.create('.'));
         	term.registerTile("dungeon.png", 5, 20, ColoredChar.create('@'));
         	term.registerTile("dungeon.png", 14, 30, ColoredChar.create('D', Color.red));
         
-
+		// Create a new Player
         	Player player = new Player(term);
+		// Generate a new World
         	World world = new Level(69, 24, player);
-        	//Screen.showFile(normalizePath("src\\rogue\\system\\start.txt","rogue/system/start.txt"), term, world);
+		// Show Splashscreen for Start
 		Screen.showFile(Path.generateAbsolutePath("rogue/system/start.txt"),term,world);
+		
+		// Press any Key to continue
         	term.getKey();
+
+		// Who deleted this, and why?
         	//world.addActor(new Monster(ColoredChar.create('D', Color.red),"roter Drache"));
+
+		// Add a Dragon so we have an enemy
 		world.addActor(new Dragon(ColoredChar.create('D',Color.red),"roter Drache"));
+		// Add Minimap to left part in Window (Size given as Parameter), focus on Player
         	term.registerCamera(player, 5, 5);
-		while(!player.expired()) {
+
+		// Play Game
+		while(!player.expired()) { // Player is still living?
+
+			// ?
 			/*Collection<Monster> monsters = world.getActorsAt(Monster.class, player.pos());
 			if(!monsters.isEmpty()){
 			    player.expire();
 			    continue;
 			}*/
-			if (roundsToHpUp == 0) {
+
+			// Finished hpCycle?
+			if (roundsToHpUp == 0) { // Yes
+				// Give Player a hitpoint
 				player.regainHitpoint();
+				// Reset Counter
 				roundsToHpUp = hpCycle;
 			}
+			// Decrement hpCycle Counter
 			roundsToHpUp--;
+
+			// Generate a List of Monsters still on Map
 			Collection<Monster> monsters = world.getActorsAt(Monster.class, player.pos());
-			if(!monsters.isEmpty()){
+			// Has every Monster been killed?
+			if(!monsters.isEmpty()){ // Yes
+				// Stop Game
 				player.expire();
 				continue;
 			}
+
+			// Redraw Windowcontents now
 			term.clearBuffer();
 			for(int x = 0; x < world.width(); x++)
 				for(int y = 0; y < world.height(); y++)
 					term.bufferChar(x + 11, y, world.look(x, y));
 			term.bufferCameras();
 			term.refreshScreen();
+
+			// Give everyone else the chance to make his move
 			world.tick();
         	}
 		term.clearBuffer();
