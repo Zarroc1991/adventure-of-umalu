b59543ccb38eab7ee7e57a64defd19100968e2cf
26thmeusoc <development@26thmeusoc.com>
2013-03-10 13:02:57 +0100

    Created File.

---
+++ src/rogue/system/System.java
@@ -0,0 +1,11 @@
+/*
+ * System.java
+ * Copyright (C) 2013 dirk <dirk@Valinor.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+public class System {
+	public static boolean debug = false;
+}
+
