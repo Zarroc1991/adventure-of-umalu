71c27b20270b7fb4949619d35916a99741cb6e38
26thmeusoc <development@26thmeusoc.com>
2013-03-10 15:46:46 +0100

    Prepared Space for Statusmessages

--- src/rogue/level/Level.java
+++ src/rogue/level/Level.java
@@ -7,17 +7,17 @@ import rogue.creature.Player;
 
 public class Level extends World
 {
-    private final static Generator gen = getLevelGenerator();
+	private final static Generator gen = getLevelGenerator();
 
-    public Level(int width, int height, Player player)
-    {
-        super(width, height);
-        gen.generate(this);
-        addActor(player);
-    }
+	public Level(int width, int height, Player player) {
+		// Create a new Map, but make it 2 Rows less higher than window is, so we have some Space for
+		// Statusmessages and stuff.
+		super(width, height-2);
+		gen.generate(this);
+		addActor(player);
+	}
 
-    private static Generator getLevelGenerator()
-    {
-        return new Cellular();
-    }
+	private static Generator getLevelGenerator() {
+		return new Cellular();
+	}
 }
