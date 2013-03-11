d3957a6c694caf57b4eee703585c22d3ca1b42d8
26thmeusoc <development@26thmeusoc.com>
2013-03-11 18:33:15 +0100

    Added Exception classes

---
+++ src/rogue/creature/util/NotEnoughGoldException.java
@@ -0,0 +1,14 @@
+/*
+ * NotEnoughGoldException.java
+ * Copyright (C) 2013 dirk <dirk@Vinyamar.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+public class NotEnoughGoldException extends Exception {
+	public NotEnoughGoldException() {
+		
+	}
+}
+
+
---
+++ src/rogue/creature/util/NotEnoughSpaceException.java
@@ -0,0 +1,14 @@
+/*
+ * NotEnoughSpaceException.java
+ * Copyright (C) 2013 dirk <dirk@Vinyamar.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+public class NotEnoughSpaceException extends Exception {
+	public NotEnoughSpaceException() {
+		
+	}
+}
+
+
