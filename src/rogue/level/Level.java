147dea20a5725b07ad2ea25b1943edd068dd6d1c
Schachus <schachus123@gmx.de>
2013-03-12 21:05:47 +0100

    BUGFIX
    fixed a bug, that caused Monsters to appear on unpassable Tiles, when
    the world is created

--- src/rogue/level/Level.java
+++ src/rogue/level/Level.java
@@ -12,41 +12,61 @@ import rogue.creature.*;
 
 import jade.util.datatype.ColoredChar;
 
-public class Level extends World
-{
-	private static Generator gen = getLevelGenerator();
+public class Level extends World {
 
-	public Level(int width, int height, Player player) {
-		// Create a new Map, but make it 2 Rows less higher than window is, so we have some Space for
-		// Statusmessages and stuff.
-		super(width, height-2);
-		gen.generate(this);
-		addActor(player);
-	}
-	// Zweiter Konstruktor, um ein neues Level aufzurufen 
-	public Level(int width, int height, Player player, int level, TiledTermPanel term) {
-		super(width, height-2);
-		switch (level){											//Liste der Maps in Abh�ngikeit vom Level  
-		case 0:{
-			gen = new World1();
-			break;	
-		}
-		case 1:{
-			gen = new World2();
-                        addActor(new Troll(term));
-                        addActor(new Orc(term));
-                        addActor(new InvisibleZombie(term));
-			addActor(new Dummy(ColoredChar.create('X',Color.blue),"Dummy",term));
+    private static Generator gen = getLevelGenerator();
 
-			break; 
-		}
-                    default:{gen = new World1();break;}
-		}
-		gen.generate(this);
-		addActor(player);
-	}
+    public Level(int width, int height, Player player) {
+        // Create a new Map, but make it 2 Rows less higher than window is, so we have some Space for
+        // Statusmessages and stuff.
+        super(width, height - 2);
+        gen.generate(this);
+        addActor(player);
+    }
+    // Zweiter Konstruktor, um ein neues Level aufzurufen
 
-	private static Generator getLevelGenerator() {
-		return new World1();
-	}
+    public Level(int width, int height, Player player, int level, TiledTermPanel term) {
+        super(width, height - 2);
+        switch (level) {											//Liste der Maps in Abh�ngikeit vom Level
+            case 0: {
+                gen = new World1();
+                break;
+            }
+            case 1: {
+                gen = new World2();
+                break;
+            }
+            default: {
+                gen = new World1();
+                break;
+            }
+        }
+        gen.generate(this);
+        addActor(player);
+        //insert Monster. We need new case-separation, because the Monsters should be added
+        //after the tile, so they do not land on unpassable tiles
+        switch (level) {											//Liste der Maps in Abh�ngikeit vom Level  
+            case 0: {
+                break;
+            }
+            case 1: {
+                addActor(new Troll(term));
+                addActor(new Orc(term));
+                addActor(new InvisibleZombie(term));
+                addActor(new Dummy(ColoredChar.create('X', Color.blue), "Dummy", term));
+                break;
+            }
+            case 2:{
+                break;
+            }
+            default:{
+                break;
+            }
+        }
+
+    }
+
+    private static Generator getLevelGenerator() {
+        return new World1();
+    }
 }
