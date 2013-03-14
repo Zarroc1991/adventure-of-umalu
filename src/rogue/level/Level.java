6236286022debc2d09ca0ba8afe22ee9f04c7ee6
26thmeusoc <development@26thmeusoc.com>
2013-03-14 19:56:09 +0100

    Removed Troll from Levels because he is massively bugged

--- src/rogue/level/Level.java
+++ src/rogue/level/Level.java
@@ -59,16 +59,16 @@ public class Level extends World {
         addActor(player);
         //insert Monster. We need new case-separation, because the Monsters should be added
         //after the tile, so they do not land on unpassable tiles
-        switch (level) {											//Liste der Maps in Abh�ngikeit vom Level  
+        switch (level) {											//Liste der Maps in Abhaengikeit vom Level  
             case 0: {
-                addActor(new Troll(term,level));
+                //addActor(new Troll(term,level));
                 addActor(new Rat(term));
                 addActor(new Slug_fat(term));
                 addActor(new Frog_poisonous(term));
                 break;
             }
             case 1: {
-                addActor(new Troll(term,level));
+                //addActor(new Troll(term,level));
                 addActor(new Rat(term));
                 addActor(new Slug_fat(term));
                 addActor(new Frog_poisonous(term));
@@ -76,7 +76,7 @@ public class Level extends World {
                 break;
             }
             case 2:{
-                addActor(new Troll(term,level));
+                //addActor(new Troll(term,level));
                 addActor(new Rat(term));//kat1
                 addActor(new Slug_fat(term));//kat2
                 addActor(new Frog_poisonous(term));//kat3
