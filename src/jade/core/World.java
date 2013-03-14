427ae5fdd23f9bde6538420def63a18bc24e2c84
Paul Kermas <kermas.paul@googlemail.com>
2013-03-14 19:29:26 +0100

    fov an

--- src/jade/core/World.java
+++ src/jade/core/World.java
@@ -597,7 +597,7 @@ public abstract class World extends Messenger
 		public Tile()			
 		{
 			passable = true;
-			viewable = true;//false -> Sihtbarkeit einschränken		//jedes Tile erhält nocht die Eigenschaft der Sichtbarkeit 
+			viewable = false;//false -> Sihtbarkeit einschränken		//jedes Tile erhält nocht die Eigenschaft der Sichtbarkeit 
 			face = ColoredChar.create('.');
 			actors = new HashSet<Actor>();
 		}
