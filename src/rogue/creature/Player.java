1954b751760f164b4c375096826878c9eebf2454
Paul Kermas <kermas.paul@googlemail.com>
2013-03-15 23:02:33 +0100

    umlaute

--- src/rogue/creature/Player.java
+++ src/rogue/creature/Player.java
@@ -104,7 +104,7 @@ public class Player extends Creature implements Camera {
 						fight((Monster) actorlist.toArray()[0]);
 					} else {
 						if (world().tileAt(x() + dir.dx(), y() + dir.dy()) == ColoredChar.create('\u00a9')) {  
-							Screen.redrawEventLine("M\u00f6chtest du diesen Raum verlassen? Dr\u00fccke j f\u00fcr Ja, ansonsten verweilst du hier.");//Stellt fest, dass eine T�r gefunden wurde und somit eine Map�nderung erfolgt
+							Screen.redrawEventLine("Möchtest du diesen Raum verlassen? Drüccke j für Ja, ansonsten verweilst du hier.");//Stellt fest, dass eine Tür gefunden wurde und somit eine Mapänderung erfolgt
 							if (term.getKey()=='j'){
 								worldchange= true;
 								move(dir);}
