b412d75ec86cad487eb5dadde50851f6d267ac96
26thmeusoc <development@26thmeusoc.com>
2013-03-18 23:08:46 +0100

    Fixed Unicodbug

--- src/rogue/creature/Unbeliever.java
+++ src/rogue/creature/Unbeliever.java
@@ -31,7 +31,7 @@ public class Unbeliever extends Monster {
     int attackRadius;
 
     public Unbeliever(Terminal term) {
-        super(ColoredChar.create('U', new Color(255,185,15)), "Ungläubiger", 21, 6, term);
+        super(ColoredChar.create('U', new Color(255,185,15)), "Ungl\u00c4ubiger", 21, 6, term);
         fov = new RayCaster();
         attackRadius = 5;
         //Typenumber 5, since cat. 5
