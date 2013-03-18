e6fd74313035a75cd4f949756ebc1d8c3b431263
Christoph Spenke <c.spenke@gmx.de>
2013-03-18 22:06:13 +0100

    leveldown in level 1 verhindern
    dementsprechend mussten auch die konstruktoren in den worlds angepasst
    werden

--- src/jade/gen/map/World1.java
+++ src/jade/gen/map/World1.java
@@ -7,12 +7,22 @@ import jade.util.Dice;
 
 // Beispiel Map World1 
 public class World1 extends NewWorld {
-
+	private int level;
+	
+	public World1(int level){
+		super();
+		this.level = level;
+		
+		 
+	}
 	@Override
 	//ruft generateStep aus NewWorld auf
 	protected void generateStep(World world, Dice dice) {
-
-		generateStep(world,dice,Path.generateAbsolutePath("maps/Welt4.txt"));
-
+		generateStep(world,dice,Path.generateAbsolutePath("maps/Welt1.txt"),level );
+		
 	}
+
+	
+	
+
 }
