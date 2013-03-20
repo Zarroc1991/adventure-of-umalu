a21fd6ddae781603026856ee4feac08d6aa6fbff
Schachus <schachus123@gmx.de>
2013-03-20 12:26:54 +0100

    simplified act in rat,(deleteted actionOver)

--- src/rogue/creature/Rat.java
+++ src/rogue/creature/Rat.java
@@ -29,9 +29,9 @@ public class Rat extends Monster {
     PathFinder pathfinder = new AStar();
     RayCaster fov;
     int attackRadius;
-   
+
     public Rat(Terminal term) {
-        super(ColoredChar.create('R', new Color(110,110,110)), "Ratte", 5, 1, term);
+        super(ColoredChar.create('R', new Color(110, 110, 110)), "Ratte", 5, 1, term);
         fov = new RayCaster();
         attackRadius = 5;
         //a Rat is the weakest Monster, hence Typenumber 1
@@ -40,70 +40,68 @@ public class Rat extends Monster {
 
     @Override
     public void act() {
-        boolean actionOver = false;
-        
+
+
         for (Direction dir : Arrays.asList(Direction.values())) {
             Player player = world().getActorAt(Player.class, x() + dir.dx(), y() + dir.dy());
 
             if (player != null) {
                 fight(player);
-
-                actionOver = true;
-                break;
-
+                return;
             }
 
         }
 
-        if (!actionOver) {
-            Collection<Coordinate> viewField = fov.getViewField(this.world(), this.pos().x(), this.pos().y(), attackRadius);
-            for (Coordinate coordinate : viewField) {
-                if (this.world().getActorAt(Player.class, coordinate) != null) {
-                    Direction dir = this.pos().directionTo(pathfinder.getPath(this.world(), this.pos(), coordinate).get(0));
-                    move(dir);
-                    actionOver = true;
-                    break;
-                }
+
+        Collection<Coordinate> viewField = fov.getViewField(this.world(), this.pos().x(), this.pos().y(), attackRadius);
+        for (Coordinate coordinate : viewField) {
+            if (this.world().getActorAt(Player.class, coordinate) != null) {
+                Direction dir = this.pos().directionTo(pathfinder.getPath(this.world(), this.pos(), coordinate).get(0));
+                move(dir);
+                return;
             }
+            move(Dice.global.choose(Arrays.asList(Direction.values())));
 
-            if (!actionOver) {
-
-
-                move(Dice.global.choose(Arrays.asList(Direction.values())));
-            }
         }
     }
 
-	@Override
-	public void fight(Player opponent) {
-		// TODO Auto-generated method stub {
-	        System.out.println("Die " + name + " greift dich an");
-		// Create Randomizer
-	        Random random = new Random();
-		// Generate Damage
-	        int abzug = random.nextInt(strength)+1;
-		// Do Damage to Oppenent
-	        opponent.loseHitpoints(abzug);
-		// Print Result
-            Random generator = new Random();
-            int ran = generator.nextInt( 4 );
-            switch(ran){
-            	case 0:System.out.println("Ihre Zähne stecken in deinem Hintern.");break;
-            	case 1:System.out.println("Sie beißt in deinen großen Zeh");break;
-            	case 2:System.out.println("Tollwutalarm");break;
-            	case 3:System.out.println("Sie quickt so laut.");break;
-	        }
-	        System.out.println("Du hast "+ abzug + " HP verloren");
-	        System.out.println("verbleibende HP: "+ opponent.hitpoints);
-		Screen.redrawEventLine(name+" macht "+abzug+" Schaden (Rest: "+opponent.hitpoints+")");
-		try {
-			term.getKey();
-		} catch(InterruptedException e) {
-			System.out.println("!InterruptedException");
-			e.printStackTrace();
-		}
-	    }
-
-	}
+    @Override
+    public void fight(Player opponent) {
+        // TODO Auto-generated method stub {
+        System.out.println("Die " + name + " greift dich an");
+        // Create Randomizer
+        Random random = new Random();
+        // Generate Damage
+        int abzug = random.nextInt(strength) + 1;
+        // Do Damage to Oppenent
+        opponent.loseHitpoints(abzug);
+        // Print Result
+        Random generator = new Random();
+        int ran = generator.nextInt(4);
+        switch (ran) {
+            case 0:
+                System.out.println("Ihre Zähne stecken in deinem Hintern.");
+                break;
+            case 1:
+                System.out.println("Sie beißt in deinen großen Zeh");
+                break;
+            case 2:
+                System.out.println("Tollwutalarm");
+                break;
+            case 3:
+                System.out.println("Sie quickt so laut.");
+                break;
+        }
+        System.out.println("Du hast " + abzug + " HP verloren");
+        System.out.println("verbleibende HP: " + opponent.hitpoints);
+        Screen.redrawEventLine(name + " macht " + abzug + " Schaden (Rest: " + opponent.hitpoints + ")");
+        try {
+            term.getKey();
+        } catch (InterruptedException e) {
+            System.out.println("!InterruptedException");
+            e.printStackTrace();
+        }
+    }
+}
 
 
